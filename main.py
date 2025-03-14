5b477aa4fec9f26e50661c37030d8c59f5b74273
J-Miazga <kuba.miazga@wp.pl>
2025-03-14 20:30:40 +0100

    Setting up the game

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame as pg
+import constant as cs
 
-print("Final test")
+pg.init()
+
+clock=pg.time.Clock()
+
+screen=pg.display.set_mode((cs.SCREEN_WIDTH,cs.SCREEN_HEIGHT))
+pg.display.set_caption("Tower Defense Game")
+run=True
+while run:
+    
+    clock.tick(cs.FPS)
+    
+    for event in pg.event.get():
+        
+        if event.type==pg.QUIT:
+            run=False
+pg.quit()
